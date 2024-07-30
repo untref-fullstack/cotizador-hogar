4c05d6d706fe58d8cabe2715edc5212fecfa04e8
Diplomatura FullStack Untref <110308301+untref-fullstack@users.noreply.github.com>
2024-07-29 22:40:19 -0300

    locations increased

--- js/variables.js
+++ js/variables.js
@@ -6,10 +6,13 @@ const datosPropiedad = [{tipo: 'Casa', factor: 1.09},
                         {tipo: 'Local Comercial', factor: 1.41},
                         {tipo: 'Depósito Logística', factor: 1.92}]
 
-const datosUbicacion = [{tipo: 'CABA', factor: 1.13},
-                        {tipo: 'Tandil', factor: 1.04},
-                        {tipo: 'Costa Atlántica', factor: 1.29},
-                        {tipo: 'Patagonia', factor: 1.00},]
+const datosUbicacion = [{ categoria: "ubicacion", tipo: "Ciudad Autónoma de Bs. As.", factor: 1.13 },
+                        { categoria: "ubicacion", tipo: "Tandil, Buenos Aires", factor: 1.04 },
+                        { categoria: "ubicacion", tipo: "Costa Atlántica", factor: 1.29 },
+                        { categoria: "ubicacion", tipo: "Patagonia Argentina", factor: 1.01 },
+                        { categoria: "ubicacion", tipo: "Gran Buenos. Aires.", factor: 1.25 },
+                        { categoria: "ubicacion", tipo: "9 de Julio, Pcia de Bs. As.", factor: 1.005 },
+                        { categoria: "ubicacion", tipo: "Chivilcoy, Pcia de Bs. As.", factor: 1.012 }]
 
 const ENV = location.href.trim()
 const URL = "js/datos.json"
