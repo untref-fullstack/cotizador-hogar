d58bfc734d821c4cf92c70ef0583e796e6ab15c1
Diplomatura FullStack Untref <110308301+untref-fullstack@users.noreply.github.com>
2024-07-29 22:48:03 -0300

    Update historial.js

--- js/historial.js
+++ js/historial.js
@@ -1,6 +1,12 @@
 const tablaHistorial = document.querySelector("tbody")
 const historialCotizaciones = JSON.parse(localStorage.getItem("historialCotizaciones")) || []
 
+historialCotizaciones.sort((a, b)=> { //ordena historial por fecha descendente
+    if (a.fechaCotizacion < b.fechaCotizacion) return -1
+    if (a.fechaCotizacion > b.fechaCotizacion) return 1
+    return 0
+})
+
 const retornoTablaHTML = (fila)=> {
     return `<tr>
                 <td>${fila.fechaCotizacion}</td>
