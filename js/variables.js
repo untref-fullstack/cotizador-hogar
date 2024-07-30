9efec1de723f0948d92482c86f1b7fbff50bf1c3
Diplomatura FullStack Untref <110308301+untref-fullstack@users.noreply.github.com>
2024-07-29 22:49:02 -0300

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -10,7 +10,7 @@ const datosUbicacion = [{ categoria: "ubicacion", tipo: "Ciudad Autónoma de Bs.
                         { categoria: "ubicacion", tipo: "Tandil, Buenos Aires", factor: 1.04 },
                         { categoria: "ubicacion", tipo: "Costa Atlántica", factor: 1.29 },
                         { categoria: "ubicacion", tipo: "Patagonia Argentina", factor: 1.01 },
-                        { categoria: "ubicacion", tipo: "Gran Buenos. Aires.", factor: 1.25 },
+                        { categoria: "ubicacion", tipo: "Gran Buenos Aires", factor: 1.25 },
                         { categoria: "ubicacion", tipo: "9 de Julio, Pcia de Bs. As.", factor: 1.005 },
                         { categoria: "ubicacion", tipo: "Chivilcoy, Pcia de Bs. As.", factor: 1.012 }]
 
